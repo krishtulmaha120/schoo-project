e28af3b691d53e95687588a0ff9dd723388fac89
krishtulmaha120 <krishtulmaha120@gmail.com>
2025-03-09 12:49:14 +0000

    Add 5.go

---
+++ src/5.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Alice"
+	age := 30
+	city := "New York"
+	fmt.Printf("My name is %s, I am %d years old and I live in %s\n", name, age, city)
+}
